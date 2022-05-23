61eb056a0e93d59b9463f84c467f23158e415e0f
Danielle Clericuzi Cavalcanti <62479830+daniclericuzi@users.noreply.github.com>
2022-05-23 20:34:04 -0300

    Update userModels.js

--- incluir-api/src/models/userModels.js
+++ incluir-api/src/models/userModels.js
@@ -1,29 +1,23 @@
 const mongoose = require('mongoose');
-const { boolean } = require('yup');
-const Schema = mongoose.Schema;
 /**
- * Atributos da entidade avaliacao
+ * Atributos da entidade user
  */
-const avaliacaoSchema = new mongoose.Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    estabelecimentoId: { type: Schema.Types.ObjectId, ref: 'Estabelecimento', required: true },
-    vagaPCD: { type: Boolean, required: true },
-    banheiro: { type: Boolean, required: true },
-    notaBanheiro: { type: Number},
-    sinalizacao: { type: Boolean, required: true },
-    notaSinalizacao: { type: Number},
-    tradutorLibras: { type: Boolean, required: true },
-    rampa: { type: Boolean, required: true },
-    locomocaoInterna: { type: Number, required: true },
-    avaliacaoGeral: { type: Number, required: true },
+const userSchema = new mongoose.Schema({
+    nome: { type: String, unique: false, required: true },
+    sobrenome: { type: String, unique: false, required: true },
+    endereco: { type: String, required: true },
+    nascimento: { type: Date, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    senha: { type: String, required: true },
   }, { timestamps: true })
 
   /**
  * Definir collection que irá ser salva no banco
  */
-  const Avaliacao = mongoose.model('Avaliacao', avaliacaoSchema);
+  const User = mongoose.model('User', userSchema);
 
    /**
- * Exportar o model Avaliacao para ser utilizado
+ * Exportar o model User para ser utilizado
  */
-module.exports = Avaliacao;
+module.exports = User;
+
