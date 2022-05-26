ad040b824c6195687c3e99401798545395d5c6e2
Danielle Clericuzi Cavalcanti <dcc@MacBook-Pro-de-Danielle-2.local>
2022-05-26 20:39:16 -0300

    finalizando controllers

--- incluir-api/src/controllers/avaliacaoController.js
+++ incluir-api/src/controllers/avaliacaoController.js
@@ -16,3 +16,56 @@ const obterAvaliacaoPorEstabelecimento = async(req, res) => {
             res.status(400).json(err);
         })
 }
+
+const realizarCadastroAvaliacao = async(req, res) => {
+    const { userId, estabelecimentoId, vagaPCD, banheiro, notaBanheiro, sinalizacao, notaSinalizacao, tradutorLibras, rampa, locomocaoInterna, avaliacaoGeral } = req.body
+    
+    try {
+        const validacaoAvaliacao = await avaliacaoSchema.validate(req.body);
+    
+        if (banheiro == false && notaBanheiro !== undefined) {
+            res.status(400).json({ message: 'Não é possível dar nota banheiro' });
+        }
+        
+        if (sinalizacao == false && notaSinalizacao !== undefined) {
+            res.status(400).json({ message: 'Não é possível dar nota Sinalizacao' });
+        }
+
+    const novaAvaliacao = new Avaliacao(validacaoAvaliacao);
+    
+    Avaliacao.find({ userId: validacaoAvaliacao.userId, estabelecimentoId: validacaoAvaliacao.estabelecimentoId  })
+    .then(async existeAvaliacao => {
+        if (existeAvaliacao !== null && existeAvaliacao.length > 0) {
+            return res.status(400).json({
+                errors: ['Já foi realizada avaliação para esse estabelecimento']
+            })
+        }
+
+    novaAvaliacao.save()
+        .then((avaliacao) => {
+            res.status(201).json(avaliacao);
+        })
+        .catch(err => next(err));
+    }) 
+} catch (e) {
+    return res.status(400).json(e)
+}
+}
+
+const deletarAvaliacao = async(req, res) =>{
+    const { id } = req.params
+     Avaliacao.findByIdAndDelete(id)
+         .then(() => {
+             res.status(200).json('Avaliação removida');
+         })
+         .catch((err) => {
+             throw new Error(err);
+         });
+ }
+
+    
+module.exports = {
+    obterAvaliacaoPorEstabelecimento,
+    realizarCadastroAvaliacao,
+    deletarAvaliacao
+    }
